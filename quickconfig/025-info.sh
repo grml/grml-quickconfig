58533063ac7cfbbb8f3b4a425542b3903424b6b5
Michael Prokop <mika@grml.org>
2012-05-09 15:04:17 +0200

    grml-info dialog: check for present HTML page and support $distri
    Thanks: Markus Ulrich for the bugreport

--- quickconfig/025-info.sh
+++ quickconfig/025-info.sh
@@ -1,11 +1,26 @@
 # grml-info module for grml-quickconfig
-LINE='print_line "Show ^information about grml (/grml-info/)"'
+if [ -n "$distri" ] ; then
+  LINE='print_line "Show ^information about $distri (/grml-info/)"'
+else
+  LINE='print_line "Show ^information about Grml (/grml-info/)"'
+fi
+
 KEY=(i)
 FUNCTION="run grml-info"
 
 display_entry() {
+  if [ -n "$distri" ] ; then
+    page="/usr/share/doc/${distri}-docs/index.html"
+  else
+    page="/usr/share/doc/grml-docs/startpage.html"
+  fi
+
+  if [ -r "$page" ] ; then
     return 0
+  else
+    return 1
+  fi
 }
 
 ## END OF FILE #################################################################
-# vim:foldmethod=marker expandtab ai ft=zsh shiftwidth=3
+# vim:foldmethod=marker expandtab ai ft=zsh shiftwidth=2
